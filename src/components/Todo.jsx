0bed0c97e95a672dfc8daee65627b62d6943cd89
hyeonky <hyeon2762@naver.com>
2024-11-22 15:38:09 +0900

    useEffect

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,20 +1,35 @@
-"use client"
+'use client'
 
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import TodoHd from './TodoHd'
 import TodoEditor from './TodoEditor'
 import TodoList from './TodoList'
-import { mockTodoData } from '@/data/todoData'
 
 const Todo = () => {
   const [todos, setTodos] = useState(mockTodoData)
+
+  // 마운트 시
+  useEffect(() => {
+    // 로컬스토리지에서 데이터를 가져와서
+    const savedTodos = JSON.parse(localStorage.getItem('todos')) || []
+    // todos 상태에 저장
+    setTodos(savedTodos)
+    console.log(savedTodos)
+  }, [])
+
+  // todos 업데이트 시
+  useEffect(() => {
+    // 로컬스토리지에 데이터를 저장
+    // JSON.stringify() : 자바스크립트 값이나 객체를 JSON 문자열로 변환
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }, [todos])
 
   const addTodo = (task) => {
     const newTodo = {
       id: todos.length + 1,
       isDone: false,
       task: task,
-      createDate: new Date().toLocaleDateString()
+      createDate: new Date().toLocaleDateString(),
     }
     setTodos([newTodo, ...todos])
   }
@@ -22,7 +37,7 @@ const Todo = () => {
   const onUpdate = (id) => {
     setTodos(
       todos.map((todo) => {
-        return todo.id === id ? {...todo, isDone: !todo.isDone} : todo
+        return todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
       })
     )
   }
@@ -32,10 +47,14 @@ const Todo = () => {
   }
 
   return (
-    <div className='flex flex-col gap-4 p-8 pb-40'>
+    <div className="flex flex-col gap-4 p-8 pb-40">
       <TodoHd />
       <TodoEditor addTodo={addTodo} />
-      <TodoList mockTodoData={todos} onUpdate={onUpdate} onDelete={onDelete} />
+      <TodoList
+        mockTodoData={todos}
+        onUpdate={onUpdate}
+        onDelete={onDelete}
+      />
     </div>
   )
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -3,32 +3,39 @@ import TodoItem from './TodoItem'
 import { set } from 'date-fns'
 
 const TodoList = ({ mockTodoData, onUpdate, onDelete }) => {
-
   const [search, setSearch] = useState('')
   const filteredTodos = () => {
-    return mockTodoData.filter(
-      (item) => item.task.toLowerCase().includes(search.toLowerCase())
-    )
+    return mockTodoData.filter((item) => item.task.toLowerCase().includes(search.toLowerCase()))
   }
 
   return (
     <div>
-        <h2>할 일 목록</h2>
-        <input 
-          type="search"
-          value={search}
-          onChange={(e) => {setSearch(e.target.value)}}
-          placeholder='검색어를 입력하세요.'
-          className='p-3 text-black w-full'
-         />
-        <ul className='mt-5 flex flex-col gap-2 divide-y'>
-          {filteredTodos().map((item) => (
+      <h2>할 일 목록</h2>
+      <input
+        type="search"
+        value={search}
+        onChange={(e) => {
+          setSearch(e.target.value)
+        }}
+        placeholder="검색어를 입력하세요."
+        className="p-3 text-black w-full"
+      />
+      <ul className="mt-5 flex flex-col gap-2 divide-y">
+        {filteredTodos().map(
+          (item) => (
             console.log(item),
-            <TodoItem key={item.id} {...item} onUpdate={onUpdate} onDelete={onDelete} />
+            (
+              <TodoItem
+                key={item.id}
+                {...item}
+                onUpdate={onUpdate}
+                onDelete={onDelete}
+              />
+            )
           )
         )}
-        </ul>
-      </div>
+      </ul>
+    </div>
   )
 }
 
