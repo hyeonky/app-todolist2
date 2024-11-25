26cb0bccdd4c007c03152055105a323dbf2d49c5
hyeonky <hyeon2762@naver.com>
2024-11-25 10:38:18 +0900

    useReducer

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,44 +1,25 @@
 'use client'
 
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useReducer, useState } from 'react'
 import TodoHd from './TodoHd'
 import TodoEditor from './TodoEditor'
 import TodoList from './TodoList'
+import { setTodos } from '@/reducers/todoReducers'
 
 const Todo = () => {
-  const [todos, setTodos] = useState([]);
+  // const [todos, setTodos] = useState([]);
+  const [todos, dispatch] = useReducer(setTodos, [])
 
-  // 마운트 시 
-  // 1. 초기 데이터 로드
-  useEffect(() => {
-      // localStorage에서 'todos' 키로 저장된 데이터를 가져옴
-      // JSON.parse() 함수를 이용하여 문자열을 객체로 변환
-      const savedTodos = JSON.parse(localStorage.getItem('todos')) || [];
-      // 가져온 데이터로 상태 업데이트
-      setTodos(savedTodos);
-  }, []) // 빈 배열: 컴포넌트가 처음 마운트될 때만 실행
+  const onDelete = (id) => {
+    dispatch({ type: 'DELETE_TODO', payload: id })
+  }
 
-  // 2. 데이터 자동 저장
   useEffect(() => {
-      // todos 상태가 변경될 때마다 localStorage에 저장
-      // JSON.stringify() 함수를 이용하여 객체를 문자열로 변환
-      localStorage.setItem('todos', JSON.stringify(todos));
-  }, [todos]) // todos가 변경될 때마다 실행
-  
-
-  // 마운트 시
-  useEffect(() => {
-    // 로컬스토리지에서 데이터를 가져와서
     const savedTodos = JSON.parse(localStorage.getItem('todos')) || []
-    // todos 상태에 저장
     setTodos(savedTodos)
-    console.log(savedTodos)
   }, [])
 
-  // todos 업데이트 시
   useEffect(() => {
-    // 로컬스토리지에 데이터를 저장
-    // JSON.stringify() : 자바스크립트 값이나 객체를 JSON 문자열로 변환
     localStorage.setItem('todos', JSON.stringify(todos))
   }, [todos])
 
@@ -58,10 +39,6 @@ const Todo = () => {
         return todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
       })
     )
-  }
-
-  const onDelete = (id) => {
-    setTodos(todos.filter((todo) => todo.id !== id))
   }
 
   return (
